3ab52512f27f6068f5df7337e493e1f80ed54be7
owhenrique <paulohenrique272@gmail.com>
2021-03-15 23:49:52 -0300

    pages app added

--- utool/settings.py
+++ utool/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'utool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # User Model
 
