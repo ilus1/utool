680622676c7bc8425a7f2d6ded1f5d5ad9f7dc3e
ilus1 <ilusdasilva@gmail.com>
2021-04-21 19:15:42 -0300

    Tools page as home

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import AboutPageView, HomePageView
+from .views import AboutPageView
+from tools.views import ToolDetailView, ToolListView
 
 app_name = "pages"
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
-    path("", HomePageView.as_view(), name="home"),
+    path("", ToolListView.as_view(), name="list"),
+    path("<slug:slug>/", ToolDetailView.as_view(), name="detail"),
+    path("category/<slug:slug>/", ToolListView.as_view(), name="list_by_category"),
 ]
