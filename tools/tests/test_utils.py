8c387858627c1d8b104daceb7168d4b170e19b0f
AdneMoretti <morettiadne1@gmail.com>
2022-08-28 22:04:00 -0300

    Little changes about test_utils and utils'

--- tools/tests/test_utils.py
+++ tools/tests/test_utils.py
@@ -8,13 +8,13 @@ class TestUtils:
         mocker.patch(
             'tools.utils.datetime'
         ).today.return_value=datetime(2022, 8, 28, 10, 9, 28, 688746)
-        date1 = datetime(2022, 8, 26, 10, 9, 28, 688746)
-        date2 = datetime(2022, 8, 28, 10, 9, 28, 688746)
+        date = datetime(2022, 8, 26, 10, 9, 28, 688746)
+        _date = datetime(2022, 8, 26, 10, 9, 28, 688746)
+        _date_ = datetime(2022, 8, 22, 10, 9, 28, 688746)
         tools_utils = ToolsUtils()
-        dias1 = tools_utils.count_days(date1, 2)
-        dias2 = tools_utils.count_days(date1, 1)
-        dias3 = tools_utils.count_days(date2, 6)
-        assert dias1 == 0
-        assert dias2 == 1
-        assert dias3 == 6
-
+        count = tools_utils.count_days(date, 2)
+        _count = tools_utils.count_days(_date, 4)
+        _count_ = tools_utils.count_days(_date_, 5)
+        assert count ==  'Hoje é a data de devolução da ferramenta'
+        assert _count ==  'A devolução é daqui a 2 dias'
+        assert _count_ ==  'A devolução está atrasada 1 dia'
--- tools/utils.py
+++ tools/utils.py
@@ -1,14 +1,18 @@
 from datetime import datetime, date, timedelta
+from faulthandler import disable
 import json
 
 class ToolsUtils:
-    def count_days(self, date_created, diasEmprestimo):
+    def count_days(self, date_created, lending_days):
+        aux='dias'
         today = datetime.today()
         days =  today-date_created
         days = days.days
-        if(days==diasEmprestimo): 
-            return 0
-        elif(days>diasEmprestimo):
-            return (days-diasEmprestimo)
+        if(days==lending_days): 
+            return 'Hoje é a data de devolução da ferramenta'
+        elif(days<lending_days and days-lending_days!=1):
+            if(lending_days-days==1): aux='dia'
+            return f'A devolução é daqui a {lending_days-days} {aux}'
         else:
-            return (diasEmprestimo-days)
+            if(days-lending_days==1): aux='dia'
+            return f'A devolução está atrasada {days-lending_days} {aux}'
